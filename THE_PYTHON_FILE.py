7e9664b83f79d3f43125e0886a2ee1531d7f33b4
Night Owl <devadevaracoolie123@gmail.com>
2025-08-06 00:23:04 +0530

    THE_PYTHON_FILE.py

---
+++ THE_PYTHON_FILE.py
@@ -0,0 +1,37 @@
+import time
+cipher = {
+    '@': 'a', ')': 'b', 'x': 'c', 'b': 'd', '(': 'e', 'v': 'f',
+    'z': 'g', '#': 'h', '^': 'i', '<': 'j', '-': 'k', '_': 'l',
+    '%': 'm', '>': 'n', '&': 'o', 'f': 'p', 'Q': 'q', '*': 'r',
+    '$': 's', ' ': 't', '/': 'u', 'u': 'v', 't': '~', '?': 'x',
+    'd': 'y', 'k': 'z', '=': ' ', '+': '?', 'h': '!', ',': ',',
+    '.': '.','j': '_'
+}
+
+while True:
+    lines = []
+    print("Enter CIPHER here \n (Type 'END'  in a new line after typing or pasting the text to reveal the output): ")
+    while True:
+        line = input()
+        if line.upper().strip() == 'END':
+            message='\n'.join(lines)
+            break
+        lines.append(line)
+    confirm = input(f"\n Is this correct? (y(yes)/n(no)): ").lower().strip()
+    if confirm == 'y':
+        pass
+        break
+    elif confirm == 'n':
+        print("\nAlright, try again.\n")
+    else:
+        print("Please type 'y' or 'n'.")
+
+for box in "███████████████████████████████████████████████████████████████████🎉" :
+    print(box, end='', flush=True)
+    time.sleep(0.02)
+
+print("\n")
+
+for box in ''.join(cipher.get(c, c) for c in message):
+    print(box, end='', flush=True)
+    time.sleep(0.09)
